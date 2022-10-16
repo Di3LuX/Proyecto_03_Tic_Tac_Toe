0dcefbb4732593c4b86b6dddb159fd9b3cb15f89
Diego.W <Di3LuX_11@hotmail.es>
2022-10-16 21:37:19 +0200

    Creando clases con el Constructor

--- JS/saveName.js
+++ JS/saveName.js
@@ -1,14 +1,26 @@
-function saveName() {
-    let name1 = document.getElementById("player1Name");
-    let name2 = document.getElementById("player2Name");
-    let player1 = sessionStorage.setItem(name1.value);
-    let player2 = sessionStorage.setItem(name2.value);
-    sessionStorage.setItem("playerXName", JSON.stringify(player1));
-    sessionStorage.setItem("playerOName", JSON.stringify(player2));
+// function saveName() {
+//     let name1 = document.getElementById("player1Name");
+//     let name2 = document.getElementById("player2Name");
+//     let player1 = sessionStorage.setItem(name1.value);
+//     let player2 = sessionStorage.setItem(name2.value);
+//     sessionStorage.setItem("playerXName", JSON.stringify(player1));
+//     sessionStorage.setItem("playerOName", JSON.stringify(player2));
 
-    let player1Data = JSON.parse(sessionStorage.getItem("playerXName"));
-    let player2Data = JSON.parse(sessionStorage.getItem("playerOName"));
+//     let player1Data = JSON.parse(sessionStorage.getItem("playerXName"));
+//     let player2Data = JSON.parse(sessionStorage.getItem("playerOName"));
 
-    player1Name.innerHTML = `Turno de ${player1Data}`;
-    player1Name.innerHTML = `Turno de ${player2Data}`;
-};
+//     player1Name.innerHTML = `Turno de ${player1Data}`;
+//     player1Name.innerHTML = `Turno de ${player2Data}`;
+// };
+
+
+// document.getElementById("player1Name").onkeyup = function(){
+//     let player1Name = this.value;
+//     sessionStorage.setItem("player1Name",name1)
+//     document.getElementById("").innerHTML=
+// }
+
+// sessionStorage.setItem("names", JSON.stringify(name1));
+
+// let saveName1 = JSON.parse(sessionStorage.getItem("names"));
+
